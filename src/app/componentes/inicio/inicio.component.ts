853ca9c74b2d45038b21b5691f5d960cd4dca2fa
newUser161 <n4k4.1184@gmail.com>
2021-11-09 08:52:45 -0300

    inicio cards

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -8,15 +8,8 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class InicioComponent implements OnInit {
 
-  activeId=1;
-  estado:boolean=false;
-  fotos=[
-    {"id":1,"nombre":"imagen1","ruta":"1.png"},
-    {"id":2,"nombre":"imagen2","ruta":"2.png"},
-    {"id":3,"nombre":"imagen3","ruta":"3.png"},
-    {"id":4,"nombre":"imagen4","ruta":"4.png"}, 
-    {"id":5,"nombre":"imagen5","ruta":"5.png"}, 
-  ];
+  active=1;
+
   
   formulario:FormGroup;
 
